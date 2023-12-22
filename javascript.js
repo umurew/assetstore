22df0b8ddfd103534f85be2732b5337b027c9a7f
umurew <87142867+umurew@users.noreply.github.com>
2023-12-22 15:09:37 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function showSnacbar(text) {
+  var x = document.getElementById("snackbar");
+  x.innerHTML = text;
+  x.className = "show";
+  setTimeout(function () {
+    x.className = x.className.replace("show", "");
+  }, 3000);
+}
